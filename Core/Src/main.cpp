dd3a1cfac1914d795e98dad3f296471223cdd7a6
Boris Mladenov Beslimov <borisbeslimov@gmail.com>
2025-12-01 13:06:49 +0100

    mdma prove

--- Core/Src/main.cpp
+++ Core/Src/main.cpp
@@ -6,11 +6,36 @@ int main(void) {
     SharedMemory::start();
 #endif
 
-    DigitalOutput led_on(PB0);
+    SCB_DisableDCache();
+
+    // DigitalOutput led_on(PB0);
     STLIB::start();
 
-    Time::register_low_precision_alarm(100, [&]() { led_on.toggle(); 
+    uint8_t data_buffer[256];
+    auto mdma_channel = MDMA::inscribe(data_buffer);
+
+    uint32_t source_data[256];
+    uint32_t destination_data[256];
+    uint32_t destination_data2[256];
+
+    for (uint32_t i = 0; i < 256; ++i) {
+        source_data[i] = i;
+        destination_data[i] = 0;
+        destination_data2[i] = 0;
+    }
+
+    auto source_node = MDMA::link_node_pool.construct(source_data, destination_data);
+    auto dest_node = MDMA::link_node_pool.construct(destination_data, destination_data2);
+    source_node->set_next(dest_node->get_node());
+
+    auto promise = Promise::inscribe();
+    MDMA::transfer_list(mdma_channel, source_node, promise);
+    promise->then([](void*){
+        while(1);
     });
+
+    // Time::register_low_precision_alarm(100, [&]() { led_on.toggle(); 
+    // });
 
     while (1) {
         STLIB::update();
